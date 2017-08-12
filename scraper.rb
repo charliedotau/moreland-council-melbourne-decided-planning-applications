75b7676a20c0c630ba5ae7be5d75b3b8875e1768
Charlie <charliedotau@users.noreply.github.com>
2017-08-12 18:48:34 +1000

    first commit

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,79 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# this script borrows heavily from https://github.com/planningalerts-scrapers/moreland
+# unlike the Planning Alerts scraper, which scrapes for new planning applications, this scraper gleans
+# planning applications for which a decision has been made (by the council)
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'mechanize'
+require 'scraperwiki'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+agent = Mechanize.new
+
+#agent.log = Logger.new "mechanize.log"
+
+
+url="https://eservices.moreland.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/ExternalRequestBroker.aspx?Module=EGELAP&Class=SUB&Type=SUBDEC"
+
+def scrape_page(page, detailpageurlprefix)
+  table = page.at("table.ContentPanel")
+  table.search("tr")[1..-1].each do |tr|
+    day, month, year = tr.search("td")[1].inner_text.split("/").map{|s| s.to_i}
+
+   # puts 
+    record = {
+      # the href is just the suffix, i.e EnquiryDetailView.aspx?Id=977780	
+      "info_url" => detailpageurlprefix + tr.at("td a").attr("href"),
+      "council_reference" => tr.at("td a").inner_text,
+      "date_received" => Date.new(year, month, day).to_s,
+      "description" => tr.search("td")[2].inner_text,
+      "address" => tr.search("td")[3].inner_text,
+      "decision" => tr.search("td")[4].inner_text,
+      "date_scraped" => Date.today.to_s
+    }
+
+    ScraperWiki.save_sqlite(['council_reference'], record)
+     
+  #  return record
+    
+  end
+end
+
+# to get to the page we're really interested in - the page with the table of planning applications - we must first
+# visit the prior page.  This ensures the relevant session cookies are setup
+
+page = agent.get(url)
+
+# move through first page 
+form = page.forms.first
+
+secondPage = form.submit(form.button_with(value: "Search"))
+#puts secondPage.uri
+#pp secondPage
+
+# Now do the paging magic
+number_pages = secondPage.at("#ctl00_MainBodyContent_mPagingControl_pageNumberLabel").inner_text.split(" ")[3].to_i
+#puts number_pages
+
+#applications = []
+
+## iterate and scrape every page
+(1..number_pages).each do |no|
+##(1..3).each do |no|
+  
+  decisionPage = agent.get("https://eservices.moreland.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquirySummaryView.aspx?PageNumber=#{no}")
+  #puts "Scraping page #{no} of results..."
+  scrape_page(decisionPage, "https://eservices.moreland.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/")
+  #puts hashRecord.to_json
+
+  
+#  applications << application
+
+end
+
+#puts applications
+
+# Setup a specific instance of an Azure::Storage::Client
+#  client = Azure::Storage::Client.create(:storage_account_name => 'your account name', :storage_access_key => 'your access key')
+
+
+# puts number_pages
+
+#agent.log = Logger.new "mechanize.log"
